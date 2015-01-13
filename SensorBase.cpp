e959d2bdf864489e6b04fcf4c77f548190eb884e
Diego Wilson <dwilson@codeaurora.org>
2015-01-13 15:40:47 -0800

    Replace entries of readdir() for readdir_r()
    readdir_r() is better suited for a multi threaded environment.
    
    Change-Id: Ibb497d78ea1690a5d14610fd6ac9372852218853

--- SensorBase.cpp
+++ SensorBase.cpp
@@ -123,7 +123,8 @@ int SensorBase::openInput(const char* inputName) {
     strlcpy(devname, dirname, PATH_MAX);
     filename = devname + strlen(devname);
     *filename++ = '/';
-    while((de = readdir(dir))) {
+    struct dirent cur;
+    while ((readdir_r(dir, &cur, &de) == 0) && (de != NULL)) {
         if(de->d_name[0] == '.' &&
                 (de->d_name[1] == '\0' ||
                         (de->d_name[1] == '.' && de->d_name[2] == '\0')))
