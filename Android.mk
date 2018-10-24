a5e582a61075e8b7ef142d3ddd591056cc2bd333
puneet <puneet@codeaurora.org>
2018-10-24 10:03:22 +0530

    hardware: qcom: sensors: Enabled bmi and iam HAL compilation
    Modified Android.mk file to compile both sensor hal code
    
    Change-Id: Ic61720d42383dac8ca9d89c371bb04dd2b90a50e
    Signed-off-by: puneet <puneet@codeaurora.org>

--- Android.mk
+++ Android.mk
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+#
+## build Invensense (IAM) HAL module
+#
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_SIMULATOR),true)
@@ -22,7 +24,7 @@ include $(CLEAR_VARS)
 ifeq ($(origin TARGET_BOARD_PLATFORM), undefined)
 	LOCAL_MODULE := sensors.default
 else
-	LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)_$(TARGET_BOARD_TYPE)
+	LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM).iam_$(TARGET_BOARD_TYPE)
 ifeq ($(PLATFORM_VERSION), 8.0.0)
 	LOCAL_VENDOR_MODULE := true
 	LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
@@ -34,28 +36,24 @@ endif
 LOCAL_MODULE_TAGS := eng
 
 LOCAL_SRC_FILES :=\
-	bmi160_iio/sensord/axis_remap.c\
-	bmi160_iio/sensord/sensord_hwcntl.cpp\
-	bmi160_iio/sensord/sensord_hwcntl_implement.cpp\
-	bmi160_iio/sensord/util_misc.c\
-	bmi160_iio/sensord/sensord_pltf.c\
-	bmi160_iio/sensord/sensord_cfg.cpp\
-	bmi160_iio/sensord/sensord_algo.cpp\
-	bmi160_iio/sensord/sensord.cpp\
-	bmi160_iio/sensord/bstsimple_list.cpp\
-	bmi160_iio/hal/sensors.cpp\
-	bmi160_iio/hal/BstSensor.cpp\
+                 ./iam20680/sensors/MPLSupport.cpp \
+                 ./iam20680/sensors/SensorsMain.cpp\
+                 ./iam20680/sensors/SensorBase.cpp\
+                 ./iam20680/sensors/tools/ml_sensor_parsing.c\
+                 ./iam20680/sensors/tools/ml_sysfs_helper.c\
+                 ./iam20680/sensors/tools/inv_sysfs_utils.c\
+                 ./iam20680/sensors/tools/inv_iio_buffer.c\
+                 ./iam20680/sensors/CompassSensor.IIO.primary.cpp\
+                 ./iam20680/sensors/MPLSensor.cpp
 
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/bmi160_iio/hal\
-		$(LOCAL_PATH)/bmi160_iio/sensord/bsx/inc\
-		$(LOCAL_PATH)/bmi160_iio/sensord/inc
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/iam20680/sensors/tools
 
-LOCAL_CFLAGS := -pthread\
+LOCAL_CFLAGS := -pthread -Wno-error=date-time\
 
 LOCAL_CPPFLAGS := -pthread\
 
-LOCAL_LDLIBS += -lm -llog -lutils
+LOCAL_LDLIBS += -lm -llog -lutils -lcutils
 #LOCAL_SHARED_LIBRARIES := libm
 #LOCAL_LDFLAGS :=
 #LOCAL_LDFLAGS_arm :=
@@ -66,11 +64,64 @@ include $(BUILD_SHARED_LIBRARY)
 
 endif  # TARGET_SIMULATOR != true
 
-#Copy bst_hal_cfg.txt to system img
+#
+## build BMI (BMI160) HAL module
+#
+ifneq ($(TARGET_SIMULATOR),true)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := bmi160_iio/bst_hal_cfg.txt
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := bmi160_iio/bst_hal_cfg.txt
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
+
+ifeq ($(origin TARGET_BOARD_PLATFORM), undefined)
+	LOCAL_MODULE := sensors.default
+else
+	LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM).bmi_$(TARGET_BOARD_TYPE)
+ifeq ($(PLATFORM_VERSION), 8.0.0)
+	LOCAL_VENDOR_MODULE := true
+	LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
+else
+	LOCAL_MODULE_RELATIVE_PATH := hw
+endif
+endif
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_SRC_FILES :=\
+		 ./bmi160/HAL_DataReady/hal/sensors.cpp \
+                 ./bmi160/HAL_DataReady/hal/BstSensor.cpp \
+                 ./bmi160/HAL_DataReady/sensord/sensord.cpp \
+                 ./bmi160/HAL_DataReady/sensord/sensord_hwcntl_implement.cpp \
+                 ./bmi160/HAL_DataReady/sensord/bstsimple_list.cpp \
+                 ./bmi160/HAL_DataReady/sensord/sensord_hwcntl.cpp \
+                 ./bmi160/HAL_DataReady/sensord/sensord_algo.cpp \
+                 ./bmi160/HAL_DataReady/sensord/sensord_cfg.cpp \
+                 ./bmi160/HAL_DataReady/sensord/sensord_pltf.c \
+                 ./bmi160/HAL_DataReady/sensord/axis_remap.c \
+                 ./bmi160/HAL_DataReady/sensord/util_misc.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/bmi160/HAL_DataReady/hal\
+		    $(LOCAL_PATH)/bmi160/HAL_DataReady/sensord/bsx/inc\
+		    $(LOCAL_PATH)/bmi160/HAL_DataReady/sensord/inc
+
+LOCAL_CFLAGS := -pthread -Wno-error=date-time\
+
+LOCAL_CPPFLAGS := -pthread\
+
+LOCAL_LDLIBS += -lm -llog -lutils -lcutils
+#LOCAL_SHARED_LIBRARIES := libm
+#LOCAL_LDFLAGS :=
+#LOCAL_LDFLAGS_arm :=
+#LOCAL_LDFLAGS_arm64 :=
+LOCAL_PRELINK_MODULE := false
+
+include $(BUILD_SHARED_LIBRARY)
+
+#Copy bst_hal_cfg.txt to system img
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := bst_hal_cfg.txt
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES := bst_hal_cfg.txt
+#LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+#include $(BUILD_PREBUILT)
+
+endif  # TARGET_SIMULATOR != true
