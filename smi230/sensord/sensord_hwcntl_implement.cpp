9b180b1f2d541eeb60f8c45d16a977d679ac7a9a
Puneet Yatnal <quic_puneety@quicinc.com>
2024-05-13 13:02:54 +0530

    hardware: qcom: sensors: fix gyro range config issue for SMI230 sensor
    GYRO range config of SMI230 is not changing if gyro sensor is suspend
    mode, gyro range change can effective only if sensor in normal mode,
    this change make ensure gyro in normal mode before changing gyro range.
    
    Change-Id: I5dcd417489e3617b0fc4889fc53dca61de761b4e

--- smi230/sensord/sensord_hwcntl_implement.cpp
+++ smi230/sensord/sensord_hwcntl_implement.cpp
@@ -309,7 +309,12 @@ uint32_t ap_get_sensorlist(struct sensor_t const** p_sSensorList)
                 break;
         }
 
-        if(MAG_CHIP_AKM09912 ==  magn_chip || MAG_CHIP_AKM09911 ==  magn_chip)
+	if(GYR_CHIP_SMI230 == gyro_chip) {
+                bosch_all_sensors[SENSORLIST_INX_GYROSCOPE].maxRange = gyro_range;
+                bosch_all_sensors[SENSORLIST_INX_GYROSCOPE_UNCALIBRATED].maxRange = gyro_range;
+	}
+
+	if(MAG_CHIP_AKM09912 ==  magn_chip || MAG_CHIP_AKM09911 ==  magn_chip)
         {
             bosch_all_sensors[SENSORLIST_INX_MAGNETIC_FIELD].name = "AKM Magnetic Field Sensor";
             bosch_all_sensors[SENSORLIST_INX_MAGNETIC_FIELD].vendor = "AKM";
@@ -2054,6 +2059,7 @@ static int32_t ap_hwcntl_init_GYRO()
     char gyro_buf_dir_name[128];
     char gyro_buffer_access[128];
     const char *gyro_device_name = NULL;
+    int32_t pwr_cfg_read;
 
     if(GYR_CHIP_BMI160 == gyro_chip)
     {
@@ -2155,6 +2161,11 @@ static int32_t ap_hwcntl_init_GYRO()
                 break;
         }
 
+	ret += rd_sysfs_oneint("pwr_cfg", gyr_input_dir_name, &pwr_cfg_read);
+	PINFO("gyro pwr_cfg to normal before setting range %d pwr_cfg_read %d", SENSOR_GYRO_PM_NORMAL, pwr_cfg_read);
+	if (pwr_cfg_read != SENSOR_GYRO_PM_NORMAL)
+		ret += wr_sysfs_oneint("pwr_cfg", gyr_input_dir_name, SENSOR_GYRO_PM_NORMAL);
+
         PINFO("gyro range config %d", gyro_range);
         switch(gyro_range){
             case GYRO_CHIP_RANGCONF_125DPS:
@@ -2175,6 +2186,9 @@ static int32_t ap_hwcntl_init_GYRO()
             default:
                 break;
         }
+	if (pwr_cfg_read != SENSOR_GYRO_PM_NORMAL)
+		ret += wr_sysfs_oneint("pwr_cfg", gyr_input_dir_name, SENSOR_GYRO_PM_SUSPEND);
+
         if (ret < 0)
         {
             PERR("write_sysfs() fail, ret = %d", ret);
