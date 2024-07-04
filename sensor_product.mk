ec7d85ede009ce63b3cbfc0230023ef465b4ffc4
Raghava Chowdam <quic_rchowdam@quicinc.com>
2024-07-04 12:31:22 +0530

    hardware: qcom: sensors: Fix compilation on Talos
    Fix compilation on Talos.
    
    Change-Id: I7f6261c20f31082b05e59eb21a18cc5b560ed3f0
    Signed-off-by: Raghava Chowdam <quic_rchowdam@quicinc.com>
    (cherry picked from commit 5085c6a94b6ca2d830662df374ee57178c2c2ad4)

--- sensor_product.mk
+++ sensor_product.mk
@@ -1,30 +1,27 @@
 ENABLE_SENSOR_CONFIGS := false
+
 ifeq ($(USE_SENSOR_HAL_VER),2.0)
-PRODUCT_PACKAGES += android.hardware.sensors@2.0-service
-PRODUCT_PACKAGES += android.hardware.sensors@2.0-impl
-PRODUCT_PACKAGES += android.hardware.sensors@2.0-service.rc
+    PRODUCT_PACKAGES += android.hardware.sensors@2.0-service
+    PRODUCT_PACKAGES += android.hardware.sensors@2.0-impl
+    PRODUCT_PACKAGES += android.hardware.sensors@2.0-service.rc
 else
 ifeq ($(ENABLE_AIDL_SENSOR),true)
-
-ifeq ($(filter $(TARGET_BOARD_PLATFORM), msmnile),$(TARGET_BOARD_PLATFORM))   #Hana metal -> msmnile_au,  msmnile_gvmq -> makena
-ifneq ($(ENABLE_HYP), true)   #msmnile_gvmq -> makena
-PRODUCT_PACKAGES += android.hardware.sensors@aidl-service.stmicroelectronics
-ENABLE_SENSOR_CONFIGS := true
+ifeq ($(filter $(TARGET_BOARD_PLATFORM),msmnile),$(TARGET_BOARD_PLATFORM))
+ifneq ($(ENABLE_HYP),true)
+      PRODUCT_PACKAGES += android.hardware.sensors@aidl-service.stmicroelectronics
+      ENABLE_SENSOR_CONFIGS := true
 endif
 endif
-
 else
 PRODUCT_PACKAGES += android.hardware.sensors-service.example
-
-else
+endif
 PRODUCT_PACKAGES += android.hardware.sensors@1.0-service
 PRODUCT_PACKAGES += android.hardware.sensors@1.0-impl
-endif # ENABLE_AIDL_SENSOR
-endif #USE_SENSOR_HAL_VER
+endif
 
-# Copy sensors config file(s)
-ifeq ($(ENABLE_SENSOR_CONFIGS), true)
-PRODUCT_COPY_FILES += \
+# Copy sensor configuration files if ENABLE_SENSOR_CONFIGS is true
+ifeq ($(ENABLE_SENSOR_CONFIGS),true)
+    PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:vendor/etc/permissions/android.hardware.sensor.accelerometer.xml \
         frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:vendor/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
         frameworks/native/data/etc/android.hardware.sensor.compass.xml:vendor/etc/permissions/android.hardware.sensor.compass.xml \
