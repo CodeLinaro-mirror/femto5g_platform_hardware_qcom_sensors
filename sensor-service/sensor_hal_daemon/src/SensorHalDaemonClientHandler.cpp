f7184b2e78e3af78b40b3e511c1edae306de331e
Raghava Chowdam <quic_rchowdam@quicinc.com>
2024-08-21 20:25:55 +0530

    hardware: qcom: sensors: Fix SHD crash during suspend/resume
    When clients are registered via the ADB command line
    and the device is suspended by disconnecting the USB,
    a crash occurs in the SHD service.
    
    This fix ensures clients are deleted gracefully,
    preventing double incrementing of the pointer.
    
    Change-Id: Id829911c831dacb7b4a789efc5f7d1cf82057128
    Signed-off-by: Raghava Chowdam <quic_rchowdam@quicinc.com>

--- sensor-service/sensor_hal_daemon/src/SensorHalDaemonClientHandler.cpp
+++ sensor-service/sensor_hal_daemon/src/SensorHalDaemonClientHandler.cpp
@@ -103,26 +103,23 @@ void SensorHalDaemonClientHandler::cleanup() {
 /******************************************************************************
 SensorHalDaemonClientHandler - Sensor API callback functions
 ******************************************************************************/
-void SensorHalDaemonClientHandler::onCapabilitiesCallback(SensorCapabilitiesMask mask) {
+bool SensorHalDaemonClientHandler::onCapabilitiesCallback(SensorCapabilitiesMask mask) {
    // please do not attempt to hold the lock, as the caller of this function
    // already holds the lock
+   bool rc = false;
    SENSOR_LOGI(LOG_TAG "--< onCapabilitiesCallback=0x%x", mask);
 
    if(strncmp(mName.c_str(),"tosomeip",sizeof(mName.c_str())) == 0) {
      myService->onCapabilitiesCallback(mask);
      return;
    }
-	   
+
    if (nullptr != mIpcSender) {
         // broadcast
         SensorAPICapabilitiesIndMsg msg(SERVICE_NAME, mask);
-        bool rc = sendMessage(msg);
-	// purge this client if failed
-	if (!rc) {
-		SENSOR_LOGE(LOG_TAG "failed rc=%d purging client=%s", rc, mName.c_str());
-		mService->deleteClientbyName(mName);
-	}
-    }
+        rc = sendMessage(msg);
+   }
+   return rc;
 }
 
 /************************************************************************************
