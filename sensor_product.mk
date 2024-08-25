68a72d388fa05dc7577886f21ec843491412e910
Raghava Chowdam <quic_rchowdam@quicinc.com>
2024-08-25 23:34:23 +0530

    hardware: qcom: sensors: fix the logic in the sensor product make file
    Fix the logic in the sensor product make file in
    order to pick default product package for talos.
    
    Change-Id: Ie3a175dd7eee53651c7e8f781ff7631b21758378
    Signed-off-by: Raghava Chowdam <quic_rchowdam@quicinc.com>

--- sensor_product.mk
+++ sensor_product.mk
@@ -1,23 +1,24 @@
 ENABLE_SENSOR_CONFIGS := false
 
 ifeq ($(USE_SENSOR_HAL_VER),2.0)
-    PRODUCT_PACKAGES += android.hardware.sensors@2.0-service
-    PRODUCT_PACKAGES += android.hardware.sensors@2.0-impl
-    PRODUCT_PACKAGES += android.hardware.sensors@2.0-service.rc
+PRODUCT_PACKAGES += android.hardware.sensors@2.0-service
+PRODUCT_PACKAGES += android.hardware.sensors@2.0-impl
+PRODUCT_PACKAGES += android.hardware.sensors@2.0-service.rc
 else
 ifeq ($(ENABLE_AIDL_SENSOR),true)
-ifeq ($(filter $(TARGET_BOARD_PLATFORM),msmnile),$(TARGET_BOARD_PLATFORM))
-ifneq ($(ENABLE_HYP),true)
-      PRODUCT_PACKAGES += android.hardware.sensors@aidl-service.stmicroelectronics
-      ENABLE_SENSOR_CONFIGS := true
-endif
+ifeq ($(filter $(TARGET_BOARD_PLATFORM), msmnile),$(TARGET_BOARD_PLATFORM))   #Hana metal -> msmnile_au,  msmnile_gvmq -> makena
+ifneq ($(ENABLE_HYP), true)   #msmnile_gvmq -> makena
+PRODUCT_PACKAGES += android.hardware.sensors@aidl-service.stmicroelectronics
+ENABLE_SENSOR_CONFIGS := true
 endif
 else
 PRODUCT_PACKAGES += android.hardware.sensors-service.example
 endif
+else
 PRODUCT_PACKAGES += android.hardware.sensors@1.0-service
 PRODUCT_PACKAGES += android.hardware.sensors@1.0-impl
-endif
+endif # ENABLE_AIDL_SENSOR
+endif #USE_SENSOR_HAL_VER
 
 # Copy sensor configuration files if ENABLE_SENSOR_CONFIGS is true
 ifeq ($(ENABLE_SENSOR_CONFIGS),true)
