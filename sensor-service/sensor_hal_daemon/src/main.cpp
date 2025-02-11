1f1db5048cc67da192c3bc0788a5a20b40a11589
Twinkle Goyal <quic_twingoya@quicinc.com>
2025-02-11 14:11:08 +0530

    hardware: qcom: sensors: Fix Klockwork Issues
    1. Pointer returned from call to function at line may be NULL and will be dereferenced
    2. Array  of size 1024 may use index value(s) 1024..ULLONG_MAX-1
    
    Change-Id: I33092c8ea0aaeb16b9780ba3be41e10286a9c968
    Signed-off-by: Twinkle Goyal <quic_twingoya@quicinc.com>

--- sensor-service/sensor_hal_daemon/src/main.cpp
+++ sensor-service/sensor_hal_daemon/src/main.cpp
@@ -167,14 +167,14 @@ void SENSOR_READ_CONF(char *file_name, configParamToRead *configParamRead)
                  break;
          }
          else if(strstr(buffer, "ENABLE_FIR=")) {
-                line = strstr(buffer, "=");
-                sscanf(&line[1], "%d", &configParamRead->EnableFIR);
-                break;
+                 line = strstr(buffer, "=");
+                 if(line) (void)sscanf(&line[1], "%d", &configParamRead->EnableFIR);
+                 break;
          }
 	 else if(strstr(buffer, "SELF_TEST=")) {
-                line = strstr(buffer, "=");
-                sscanf(&line[1], "%d", &configParamRead->SensorSelfTest);
-                break;
+                 line = strstr(buffer, "=");
+                 if(line) (void)sscanf(&line[1], "%d", &configParamRead->SensorSelfTest);
+                 break;
          }
        }
     }
