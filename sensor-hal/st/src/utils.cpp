52daead95b085132cf9a41f78f7f845cbee7d229
Raghava Chowdam <quic_rchowdam@quicinc.com>
2025-03-05 16:53:03 +0000

    hardware: qcom: sensors: Improve floating-point precision in setting the range
    Changed format specifier from %f to %.9f to ensure higher precision
    for floating-point values.
    
     This change addresses the issue of truncated decimal places, ensuring
    accurate representation of values up to 9 decimal places.
    
    Change-Id: I5a66d9d480a23a673139fa33833c0b135ca153c2
    Signed-off-by: Raghava Chowdam <quic_rchowdam@quicinc.com>

--- sensor-hal/st/src/utils.cpp
+++ sensor-hal/st/src/utils.cpp
@@ -95,7 +95,7 @@ int device_iio_utils::sysfs_write_scale(char *file, float val)
 	if (NULL == fp)
 		return -errno;
 
-	fprintf(fp, "%f", val);
+	fprintf(fp, "%.9f", val);
 	fclose(fp);
 
 	return 0;
