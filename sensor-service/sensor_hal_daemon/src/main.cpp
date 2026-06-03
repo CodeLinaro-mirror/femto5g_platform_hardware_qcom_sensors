92a99b6fe790b6eac979ace3a0db94fe2d1accc4
Raghava Chowdam <rchowdam@qti.qualcomm.com>
2026-06-03 11:01:11 +0530

    hardware: qcom: sensors: properly delete callback arrays in destroy
    Add delete[] calls for mSensorTrackingOption and mSensorMLCEventCbs arrays to prevent use-after-free crashes during shutdown. These arrays were being allocated in onReceive() but never freed, causing the listener thread to access freed memory during the cleanup phase.
    
    This fixes a SIGBUS crash observed when sensord terminates, especially
    during graceful shutdown scenarios.
    
    Change-Id: I5a81f4e3844b92bc599d04dc26c58373b31c4fc3
    Signed-off-by: Raghava Chowdam <rchowdam@qti.qualcomm.com>

--- sensor-service/sensor_hal_daemon/src/main.cpp
+++ sensor-service/sensor_hal_daemon/src/main.cpp
@@ -41,11 +41,12 @@
 #include <unistd.h>
 #include <sys/prctl.h>
 #include <sys/capability.h>
+#include <atomic>
 #include <SensorApiService.h>
 
 #define HAL_DAEMON_VERSION "1.1.0"
 
-bool shutdown_due_to_sigterm = false;
+std::atomic<bool> shutdown_due_to_sigterm{false};
 
 // this function will block until the directory specified in
 // dirName has been created
@@ -215,7 +216,7 @@ static void* sigterm_wait_thread(void*)
     int rc = sigwait(&set, &sig);
     if(rc == 0 && sig == SIGTERM){
 	SENSOR_LOGI(LOG_TAG "SIGTERM received, shutting down...\n");
-	shutdown_due_to_sigterm = true;
+	shutdown_due_to_sigterm.store(true, std::memory_order_release);
 	SensorApiService::requestStop();
     }
     else
@@ -254,7 +255,7 @@ int main(int argc, char *argv[])
         SensorApiService::requestStop();
 
     // If shutdown was NOT caused by SIGTERM, cancel sigwait thread
-    if (!shutdown_due_to_sigterm && rc == true)
+    if (!shutdown_due_to_sigterm.load(std::memory_order_acquire) && rc == true)
         pthread_cancel(sig_thread);
 
     if (rc == true)
